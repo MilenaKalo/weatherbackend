3a05abf795dbe3303a05e3b55ab5247a5f4273be
milena <milenakalo@icloud.com>
2022-06-15 13:44:49 +0200

    Test added

--- src/main/java/htw/berlin/weatherbackend/controller/WeatherOfCityRest.java
+++ src/main/java/htw/berlin/weatherbackend/controller/WeatherOfCityRest.java
@@ -36,7 +36,7 @@ public class WeatherOfCityRest {
     @PostMapping(path = "/api/v1/weatherofcity")
     public ResponseEntity<Void> createCity( @Valid @RequestBody CityManipulationRequest request) throws URISyntaxException {
         var city = cityService.create(request);
-        URI uri = new URI("/api/v1/persons/" + city.getId());
+        URI uri = new URI("/api/v1/weatherofcity/" + city.getId());
         return ResponseEntity
                 .created(uri)
                 .header("Access-Control-Expose-Headers", "Location")
